f62e53aa3d81f26c9ffd7417e8dba7018b5e5eb3
David <david.richard@dritconsulting.ca>
2022-10-27 14:48:27 -0400

    add proposalStarted listener

--- src/listener/eventListener.ts
+++ src/listener/eventListener.ts
@@ -3,9 +3,11 @@ import { lunarHQ_url } from "../../config.json";
 import { readFileSync } from "fs";
 import { io } from "socket.io-client";
 import { archiveProposal } from "../utils/archiveProposal";
-import { BaseGuildTextChannel, Guild, GuildBasedChannel, Message } from "discord.js";
+import { BaseGuildTextChannel, Guild, GuildBasedChannel, GuildMember, GuildPremiumTier, Message, TextChannel } from "discord.js";
 import { Proposal } from "../shared/apiTypes";
 import { LunarAssistant } from "..";
+import { primaryEmbed, proposalEmbed } from "../utils/embeds";
+import { castProposalVoteButtons } from "../utils/buttons";
 const logger = require('../logging/logger');
 
 /*
@@ -83,6 +85,75 @@ export async function StartListener(lunarAssistant: LunarAssistant) {
 
         callback("ok");
     });
+
+    socket.on("proposalStarted", async (proposal: Proposal, callback) => {
+      logger.info(`Received proposalStarted event with args: ${JSON.stringify(proposal)}`);
+
+      let guild: Guild;
+      try {
+        guild =
+        lunarAssistant.client.guilds.cache.get(proposal.discordServerId) ??
+          (await lunarAssistant.client.guilds.fetch(proposal.discordServerId));
+        if (!guild) return;
+      } catch (e) {
+          return;
+      }
+
+      let channel: GuildBasedChannel | null;
+        try {
+          channel =
+            guild.channels.cache.get(proposal.discordChannelId!) ??
+            (await guild.channels.fetch(proposal.discordChannelId!));
+          if (!channel || !(channel instanceof BaseGuildTextChannel)) return;
+        } catch (e) {
+          logger.error(e);
+          return;
+        }
+
+        let member: GuildMember | null;
+        try {
+          member =
+            guild.members.cache.get(proposal.creatorDiscordId!) ??
+            (await guild.members.fetch(proposal.creatorDiscordId!));
+          if (!member) return;
+        } catch (e) {
+          logger.error(e);
+          return;
+        }
+
+      let message: Message;
+      try {
+        message = await channel.send({
+          embeds: [proposalEmbed(proposal, member.user)],
+          components: [castProposalVoteButtons()],
+        });
+      } catch (error) {
+        logger.error(`proposalStarted: Failed to create proposal embed for proposalId: ${proposal.id}.`);
+        return;
+      }
+      
+      if (message.channel instanceof TextChannel) {
+        try {
+          await message.channel.threads.create({
+            startMessage: message.id,
+            name: `Discussion - ${proposal.title}`,
+            autoArchiveDuration:
+              message.guild!.premiumTier === GuildPremiumTier.None ? 1440 : 4320,
+          });
+        } catch (error) {
+          await message.reply({
+            embeds: [
+              primaryEmbed(
+                "Could not create a thread",
+                "Please make sure I have permission to create threads."
+              ),
+            ],
+          });
+        }
+      }
+
+      callback("ok");
+  });
 
     socket.connect();
 }
